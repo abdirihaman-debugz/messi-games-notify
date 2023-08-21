3102ca99085055d6fe69f52410ce2d21b350a5ae
a <aabdirihaman@gmail.com>
2023-08-20 23:18:07 -0700

    added some table data checks

--- messiGamesNotify.py
+++ messiGamesNotify.py
@@ -4,13 +4,13 @@ from bs4 import BeautifulSoup
 from prettytable import PrettyTable
 from datetime import datetime
 
-#Get's the current month & appends it to the URL 
+# Get's the current month & appends it to the URL 
 month = datetime.now().strftime("%B")
 
-#Fox sports website that pull the inter-miami team schedule (when I scrape I just get the current months schedule)
-URL  = "https://www.foxsports.com/soccer/inter-miami-cf-team-schedule?month=" + str(month)
+# Fox sports website that pull the inter-miami team schedule (when I scrape I just get the current months schedule)
+URL  = "https://www.foxsports.com/soccer/inter-miami-cf-team-schedule?month=" + month
 
-#mandatory libraries I am using to extract the html & dom tree so I can use look for specific Xpaths
+# mandatory libraries I am using to extract the html & dom tree so I can use look for specific Xpaths
 page = requests.get(URL)
 soup = BeautifulSoup(page.text, "html.parser")
 dom =  etree.HTML(str(soup))
@@ -20,7 +20,7 @@ i = 1
 dataFromXpath=[]
 while(True):
     try:
-        #After a while of scraping and looking this was the smartest way or laziest way to iterate over the data to get the game time, game day, and team they are facing
+        # After a while of scraping and looking this was the smartest way or laziest way to iterate over the data to get the game time, game day, and team they are facing
         trdata = "tr["+str(i)+"]"
         teamName = dom.xpath('//*[@id="table-1"]/tbody/' +trdata+ '/td[2]/div/a[2]')[0].text.strip()
         dataFromXpath.append(teamName)
@@ -38,12 +38,19 @@ def divide_chunks(l, n):
     for i in range(0, len(l), n): 
         yield l[i:i + n]
 
-#We are using a prettyTable library to create a nice table
-#this creates the headers for table.
-myTable = PrettyTable(["The Team InterMiami is facing", "The Date", "UTC TIME"]) #future work - add library to change UTC to current time.
+# We are using a prettyTable library to create a nice table
+# this creates the headers for table.
+dataTable = PrettyTable(["The Team InterMiami is facing", "The Date", "UTC TIME"]) #future work - add library to change UTC to current time.
 
-#Adds the data to the table.
+# Adds the data to the table.
 dataRows = list(divide_chunks(dataFromXpath, 3))
-myTable.add_rows(dataRows)
+dataTable.add_rows(dataRows)
 
-print(myTable)
+try:
+    ## Might be a poor man's solution but, this is my way of checking if the table actually has data when printing or if the scrapper is broken.
+    if dataTable[0][0]:
+        print ("*******************************************************************" + " INTER-MIAMI'S SCHEDULE " + "*******************************************************************")
+        print(dataTable)
+        print ("*******************************************************************" + " LET's GOO!!! & HAPPY WATCHING " + "*******************************************************************")
+except:
+    print ("*******************************************************************" + "SORRY SCRAPPER NEEDS UPDATING OR IS BROKEN" + "*******************************************************************")
